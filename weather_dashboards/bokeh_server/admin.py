16f14b3b1eb7ef18eac869ff3bb1d433b059198e
Alex Hilson <alexhilsonwork@googlemail.com>
2018-01-18 18:33:14 +0000

    Add database models for storing remote dashboard urls

--- weather_dashboards/bokeh_server/admin.py
+++ weather_dashboards/bokeh_server/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, Dashboard
+
+admin.site.register(Group)
+admin.site.register(Dashboard) 
--- weather_dashboards/bokeh_server/models.py
+++ weather_dashboards/bokeh_server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField('Dashboard Group', max_length=20)
+
+class Dashboard(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    url = models.CharField('Source URL', max_length=200)
