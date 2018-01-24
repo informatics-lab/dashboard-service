c5f25a7fc73b70755de07aebec82f306a734e9cc
Alex Hilson <alexhilsonwork@googlemail.com>
2018-01-24 13:40:06 +0000

    Restructure project to use docker-compose

--- django_dashboards/weather_dashboards/settings.py
+++ django_dashboards/weather_dashboards/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b*a=1u_vn+r$agqs_=izt+yli!v)404t$do)(d$tzsb4mdn-5@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,4 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
